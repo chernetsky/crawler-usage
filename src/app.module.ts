6b836b58d5d187c5e2f3b765d9c8aa4f9232936b
Sergey Chernetsky <chernetsky@gmail.com>
2022-06-08 17:07:29 +0800

    Common logger done

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CrawlerModule } from 'crawler';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
   imports: [
+    LoggerModule,
     CrawlerModule.forRoot({
       min: -10,
       max: 10,
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-// import { RandomService } from 'crawler';
 
 @Injectable()
 export class AppService {
